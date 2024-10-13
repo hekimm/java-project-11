9e0554ade98d10c6cec889f7d11e06764b2dcd43
HEKİMCAN AKTAŞ <hekimcanaktas@gmail.com>
2024-10-13 23:20:46 +0300

    Initial commit

---
+++ JavaApplication33/src/javaapplication33/JavaApplication33.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication33;
+
+/**
+ *
+ * @author hekimcanaktas
+ */
+public class JavaApplication33 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int i,j;
+        
+        for(i=1;i<=10;i++){
+            System.out.println("****************************");
+            for(j=1;j<=10;j++){
+                System.out.println(i + " x " + j + " = " + (i*j));
+                
+
+            }
+        }
+
+
+
+
+    }
+    
+}
